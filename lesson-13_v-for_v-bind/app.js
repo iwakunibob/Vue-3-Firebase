58992c96aaca391a6eb2667bec1d0a6b50e4e8b9
iwakunibob <rml128+github@gmail.com>
2021-02-02 02:21:34 +0900

    kk

---
+++ lesson-13_v-for_v-bind/app.js
@@ -0,0 +1,23 @@
+const app = Vue.createApp({
+  data() {
+    return {
+      url: 'https://www.thenetninja.co.uk',
+      showBooks: true,
+      books: [
+        { title: 'name of the wind', author: 'patrick rothfuss', img: 'assets/1.jpg', isFav: true },
+        { title: 'the way of kings', author: 'brandon sanderson', img: 'assets/2.jpg', isFav: true },
+        { title: 'the final empire', author: 'brandon sanderson', img: 'assets/3.jpg', isFav: false },
+      ],
+    }
+  },
+  methods: {
+    toggleShowBooks() {
+      this.showBooks =  !this.showBooks
+    },
+    toggleFav(book) {
+      book.isFav = !book.isFav
+    }
+  }
+})
+
+app.mount('#app')
