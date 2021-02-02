fbf7db969625d866afb0ca3998faa0306087c246
iwakunibob <rml128+github@gmail.com>
2021-02-02 21:28:25 +0900

    hh

--- lesson-13_v-for_v-bind/app.js
+++ lesson-13_v-for_v-bind/app.js
@@ -6,7 +6,7 @@ const app = Vue.createApp({
       books: [
         { title: 'name of the wind', author: 'patrick rothfuss', img: 'assets/1.jpg', isFav: true },
         { title: 'the way of kings', author: 'brandon sanderson', img: 'assets/2.jpg', isFav: true },
-        { title: 'the final empire', author: 'brandon sanderson', img: 'assets/3.jpg', isFav: false },
+        { title: 'the final empire', author: 'Robert Laurie', img: 'assets/3.jpg', isFav: false },
       ],
     }
   },
@@ -16,6 +16,11 @@ const app = Vue.createApp({
     },
     toggleFav(book) {
       book.isFav = !book.isFav
+    }
+  },
+  computed: {
+    filteredBooks() {
+      return this.books.filter(book => book.isFav)
     }
   }
 })
